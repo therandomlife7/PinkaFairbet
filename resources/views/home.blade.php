aee485db305a1a92e39e05e5f36c6b60fd221835
therandomlife7 <therandomlife7@gmail.com>
2020-06-06 09:59:03 +0530

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'FairBet')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">You are logged in!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="test">
+        <h4>My name is test I am Made by Pinka - The Super Author </h4> 
+
+    </div>
+@stop
